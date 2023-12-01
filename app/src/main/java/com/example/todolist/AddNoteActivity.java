3a817758b5e704c3bc8d526a55271f1449ba05cb
BIBLETUM <pasha.remizov@mail.ru>
2023-12-01 15:41:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -0,0 +1,82 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+
+public class AddNoteActivity extends AppCompatActivity {
+    private DataBase dataBase = DataBase.getInstance();
+    private EditText textViewNoteText;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private RadioButton radioButtonHigh;
+    private Button buttonSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        initViews();
+
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+
+    }
+
+    public void initViews() {
+        textViewNoteText = findViewById(R.id.textViewNoteText);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        radioButtonHigh = findViewById(R.id.radioButtonHigh);
+        buttonSave = findViewById(R.id.buttonSave);
+    }
+
+    private void saveNote() {
+        String text = textViewNoteText.getText().toString().trim();
+        if (text.isEmpty()) {
+            Toast.makeText(
+                    this,
+                    "Введите текст заметки!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        } else {
+            int priority = getPriority();
+            int id = dataBase.getNotes().size();
+            Note note = new Note(id, priority, text);
+            dataBase.add(note);
+
+            finish();
+        }
+    }
+
+    private int getPriority() {
+        if (radioButtonLow.isChecked()) {
+            return 0;
+        } else if (radioButtonMedium.isChecked()) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    public static Intent newIntent(Context context) {
+        Intent intent = new Intent(context, AddNoteActivity.class);
+        return intent;
+    }
+
+}
---
+++ app/src/main/java/com/example/todolist/Note.java
@@ -0,0 +1,25 @@
+package com.example.todolist;
+
+public class Note {
+    private int id;
+    private int priority;
+    private String text;
+
+    public Note(int id, int priority, String text) {
+        this.id = id;
+        this.priority = priority;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
